137956e595d92d2579e147551c5a33e41f2808b9
Oshitha <osithadil55@gmail.com>
2025-04-24 12:52:13 +0530

    update code delete function ✏

--- src/api/health.ts
+++
@@ -1,9 +0,0 @@
-import express from "express"; 
-import { CreateData, GetAllData, getDataById, updateData } from "../controllers/health";
-
-const rotue = express.Router(); 
-
-rotue.route("/").get(GetAllData) .post(CreateData);
-rotue.route("/:_id").get(getDataById).put(updateData);
-
-// export default rotue; 
--- src/api/userDetail.ts
+++ src/api/userDetail.ts
@@ -1,8 +1,9 @@
 import express from "express"; 
-import { CreateUser, GetAllUserData } from "../controllers/userDetail";
+import { CreateUser, DeleteUser, GetAllUserData, getUserDataById} from "../controllers/userDetail";
 
 const rotue = express.Router();
 
 rotue.route("/").get(GetAllUserData).post(CreateUser); 
+rotue.route("/:_id").get(getUserDataById).delete(DeleteUser);
 
 export default rotue; 
