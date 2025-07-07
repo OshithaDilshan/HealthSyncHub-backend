1f5f6af107bfea7369390d2408d9b2728d515342
yasithSaparamadu <yasithsaparamadu94@gmail.com>
2025-07-08 00:48:28 +0530

    meal plan change

--- routes/meal.js
+++ routes/meal.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/authMiddleware'); // Ensure this path is correct
 
-const { GenerateMeal, GetAllMealData, GetTodayMealPlan } = require('../controllers/mealController');
+const { GenerateMeal, GetAllMealData, GetTodayMealPlan, GetRandomMealPlan, GetMealsByCalorieRange } = require('../controllers/mealController');
 
-// Public routes
 router.route('/')
-  .get(GetAllMealData)
-  .post(GenerateMeal);
+  .get(GetAllMealData) 
+  .post(GenerateMeal); 
+router.get('/today', protect, GetTodayMealPlan); 
+router.get('/random', protect, GetRandomMealPlan); 
+router.get('/range', protect, GetMealsByCalorieRange); 
 
-router.get('/today', protect, GetTodayMealPlan);
 
 module.exports = router;
